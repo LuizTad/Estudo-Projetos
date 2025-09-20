06ecef417e230778caeeb7b182ccffb1372c5f20
unknown <luiztad2002@gmail.com>
2025-09-20 19:32:55 -0300

    Correção - Atualização 2025

--- Jogo de dados/jogo.php
+++ Jogo de dados/jogo.php
@@ -1,60 +1,55 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jogo</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-
 <?php
-    session_start();
-
-    $_SESSION["player1"];
-    $_SESSION["player2"];
-    echo "<h2>Vamos jogar, <u>".$_SESSION["player1"]."</u> e <u>".$_SESSION["player2"]."</u> !</h2>";
+session_start();
 ?>
 
-
+<!DOCTYPE html>
 <html lang="pt-BR">
 
-
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-escale=1.0">
-    <title>Jogo de Dados</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jogo de Dados</title>
+  <link rel="stylesheet" href="styleJogo.css">
 </head>
 
-
 <body>
+  <div class="container">
+    <?php
+    echo "<h2>Vamos jogar, <u>{$_SESSION["player1"]}</u> e <u>{$_SESSION["player2"]}</u>!</h2>";
+    ?>
+
     <form action="jogo.php" method="post">
+      <button type="submit" name="btn-rand" class="btn">🎲 Jogar Dados!</button>
+    </form><br>
 
-        <input type="submit" name="btn-rand" value="Jogar dados!">
+    <?php
+    if (isset($_POST["btn-rand"])) {
+      $dado1 = rand(1, 6);
+      $dado2 = rand(1, 6);
 
-    </form>
+      echo "<div class='resultado'>";
+      echo "<div class='linha'><strong>{$_SESSION["player1"]}</strong> tirou <strong><span class='dado'>{$dado1}</span></strong></div>";
+      echo "<div class='linha'><strong>{$_SESSION["player2"]}</strong> tirou <strong><span class='dado'>{$dado2}</span></strong></div>";
 
-
-
-
-<?php
-    $dado1 = rand(0, 10);
-    $dado2 = rand(0, 10);
-
-    if(isset($_POST["btn-rand"])){
-        echo "<br>Resultado do <strong>".$_SESSION["player1"]."</strong> é: ".$dado1;
-        echo "<br>Resultado do <strong>".$_SESSION["player2"]."</strong> é: ".$dado2."<br>";
+      if ($dado1 > $dado2) {
+        echo "<strong><p class='vencedor'>🏆 {$_SESSION["player1"]} venceu!</p></strong>";
+      } elseif ($dado2 > $dado1) {
+        echo "<strong><p class='vencedor'>🏆 {$_SESSION["player2"]} venceu!</p></strong>";
+      } else {
+        echo "<strong><p class='empate'>🤝 Empate!</p></strong>";
+      }
+      echo "</div>";
     }
+    ?>
 
-?>
-    <br>
-    <input type="submit" value="Trocar os nomes" id="trocarNome">
+    <button id="trocarNome" class="btn">🔄 Trocar os nomes</button>
+  </div>
 
-</body>
-</html>
-
-<script type="text/javascript">
+  <script>
     document.getElementById("trocarNome").onclick = function () {
-        location.href = "http://localhost/Jogo de dados/inicio.php";
+      location.href = "inicio.php";
     };
-</script>
+  </script>
+</body>
+
+</html>
